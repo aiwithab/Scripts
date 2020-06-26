8d9d1afe33f8d53181c6abdc96e08c30c63de656
Mohammed Sharique Ansari <ansari.sharique.75@gmail.com>
2020-06-26 20:47:08 +0530

    New script for mouse click added

---
+++ RandomMouseClick.py
@@ -0,0 +1,16 @@
+import win32api
+import win32con
+
+
+import random
+import time
+
+
+# x_dim, y_dim = mouse.screen_size()
+
+while True:
+
+    win32api.SetCursorPos((random.randint(200, 400), random.randint(400, 550)))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, random.randint(1, 1000), random.randint(1, 1000), 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, random.randint(1, 1000), random.randint(1, 1000), 0, 0)
+    time.sleep(3)

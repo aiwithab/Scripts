6208bc7d6a017c1409ebe5e0a795a71930e279b8
aiwithab <abdulahadas@gmail.com>
2020-06-27 17:34:36 +0530

    fixes #3

--- randomMouseClickGUI.py
+++ randomMouseClickGUI.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+
+import win32api
+import win32con
+
+
+import random
+import time
     
 defaultTime = 2
 defaultInitialXCoordinate = 20
@@ -8,15 +15,39 @@ defaultFinalYCoordinate = 400
 
 def setSleepTime(userInputTime):
     print(f"time set to {userInputTime}")
-    defaultTime = userInputTime
+    
 
+def getTime():
+    defaultTime = timeEntry.get()
+    setSleepTime(defaultTime)
+
+def getCoordinates():
+    defaultInitialXCoordinate = initialXCoordinateEntry.get()
+    defaultInitialYCoordinate = initialYCoordinateEntry.get()
+    defaultFinalXCoordinate = finalXCoordinateEntry.get()
+    defaultFinalYCoordinate = finalYCoordinateEntry.get()
+
+    setMouseRectangleCoordinates(defaultInitialXCoordinate,defaultInitialYCoordinate,defaultFinalXCoordinate,defaultFinalYCoordinate)
 
 def setMouseRectangleCoordinates(initialXCoordinate,initialYCoordinate,finalXCoordinate,finalYCoordinate,):
     print(f"cordinates set to {initialXCoordinate} {initialYCoordinate} {finalXCoordinate} {finalYCoordinate}")
-    defaultInitialXCoordinate = initialXCoordinate
-    defaultInitialYCoordinate = initialYCoordinate
-    defaultFinalXCoordinate = finalXCoordinate
-    defaultFinalYCoordinate = finalYCoordinate
+   
+
+
+def runMainScript():
+    
+    while True:
+
+        win32api.SetCursorPos((random.randint(defaultInitialXCoordinate, defaultFinalXCoordinate), random.randint(defaultInitialYCoordinate, defaultFinalXCoordinate)))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, random.randint(1, 1000), random.randint(1, 1000), 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, random.randint(1, 1000), random.randint(1, 1000), 0, 0)
+        time.sleep(defaultTime)
+
+def stopMainScript():
+    
+    print('stopped!!!!')
+    exit()
+
 
 root = tk.Tk()
 root.title('Random Mouse Click')
@@ -30,8 +61,8 @@ timeLabel.pack()
 timeEntry.pack()
 timeButton = tk.Button(frame, 
                    text="SET SLEEP TIME", 
-                   fg="red",
-                   command=setSleepTime)
+                   fg="blue",
+                   command=getTime)
 timeButton.pack()
 timeSettedLabel = tk.Label(frame, text=f"Time is set to {defaultTime} seconds")
 timeSettedLabel.pack()
@@ -59,10 +90,18 @@ finalYCoordinateEntry = tk.Entry(frame)
 finalYCoordinateLabel.pack()
 finalYCoordinateEntry.pack()
 # BUTTON
-coordinateButton = tk.Button(frame,
+coordinateButton = tk.Button(frame,fg='blue',
                    text="SET CORDINATES",
-                   command=setMouseRectangleCoordinates)
+                   command=getCoordinates)
 coordinateButton.pack()
 coordinateSettedLabel = tk.Label(frame,text=f"Coordiantes are set to initial x = {defaultInitialXCoordinate} y = {defaultInitialYCoordinate} and final x = {defaultFinalXCoordinate} y = {defaultFinalYCoordinate}")
 coordinateSettedLabel.pack()
+
+
+startScriptButton = tk.Button(frame,fg='green',text="Start Script",command=runMainScript)
+startScriptButton.pack()
+stopScriptButton = tk.Button(frame,fg='red',text="Stop Script",command=stopMainScript)
+stopScriptButton.pack()
+
 root.mainloop()
+
